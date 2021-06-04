1b98607253712ca66a75da5ac35281543f7cfcb0
Amneh-momani <Amneh99el@gmail.com>
2021-06-03 21:02:09 -0700

    last changes

--- js/app.js
+++ js/app.js
@@ -359,36 +359,34 @@ salesform.addEventListener('submit', saveChanges);
 
 function saveChanges(event) {
   event.preventDefault();
-  // console.log(event);
 
   let ShopName = event.target.nameField.value;
 
-  let mincust = event.target.numberField.value;
-  let maxcust = event.target.maxnumberField.value;
-  let avgcookie = event.target.avgnumberField.value;
 
-  // let addShop = new Country(ShopName, mincust, maxcust, avgcookie, [], [], 0);
+  let mincust = parseInt(event.target.numberField.value);
+  let maxcust = parseInt(event.target.maxnumberField.value);
+  let avgcookie = parseFloat(event.target.avgnumberField.value);
 
-  // console.log(addShop);
-  if (mincust > maxcust) {
-    alert("please input number less than maxcust");
-    mincust.value="";
+
+  if (mincust >= maxcust) {
+    alert('please input number less than maxcust');
+    event.target.numberField.value = '';
   }
-else {
-  let addShop = new Country(ShopName, mincust, maxcust, avgcookie, [], [], 0);
+  else {
+    let addShop = new Country(ShopName, mincust, maxcust, avgcookie, [], [], 0);
 
-  table.textContent = '';
+    table.textContent = '';
 
-  makingHeader();
+    makingHeader();
 
-  for (let y = 0; y < totalHours.length; y++) {
-    totalHours[y].custNum();
-    totalHours[y].sumFun();
-    totalHours[y].render();
+    for (let y = 0; y < totalHours.length; y++) {
+      totalHours[y].custNum();
+      totalHours[y].sumFun();
+      totalHours[y].render();
+    }
+
+    makingFooter();
   }
-
-  makingFooter();
-}
   // let avgcookie = event.target.avgnumberField.value;
 
   // let addShop = new Country(ShopName, mincust, maxcust, avgcookie, [], [], 0);
